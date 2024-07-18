b9e21a8500feeaf812471db61404178f203616fa
Sri Deekshitha Devarakonda <138136318+SriDeekshithaDevarakonda@users.noreply.github.com>
2024-07-18 19:01:02 +0530

    auth.service.ts

---
+++ auth.service.ts
@@ -0,0 +1,35 @@
+//Implement user signup and login in auth.service.ts:
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user.schema';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async signup(username: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.userService.create(username, hashedPassword);
+  }
+}
