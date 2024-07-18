d10de48005ee36b5eafca5f4cda70c841d3e5003
Sri Deekshitha Devarakonda <138136318+SriDeekshithaDevarakonda@users.noreply.github.com>
2024-07-18 19:17:20 +0530

    auth.module.ts

---
+++ auth.module.ts
@@ -0,0 +1,22 @@
+//Use JwtModule in auth.module.ts:
+
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'your_jwt_secret',
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
